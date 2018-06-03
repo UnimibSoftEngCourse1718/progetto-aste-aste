cc4786f0994a443a7c0e79e7c23f2da9dd378fd0
Unknown <s.fratea2@campus.unimib.it>
2018-06-04 01:11:23 +0200

    FE aggiungiOGG

--- src/main/webapp/js/controllers/oggettoController.js
+++ src/main/webapp/js/controllers/oggettoController.js
@@ -3,7 +3,8 @@ app.controller('oggettoController', [
 	'$window',
 	'oggettoService',
 	'categoriaService',
-	function($scope, $window, oggettoService,categoriaService) {
+	'userService',
+	function($scope, $window, oggettoService,categoriaService,userService) {
 		
 		$scope.listCategoria = [];
 		$scope.listAttributi = [];
@@ -25,8 +26,23 @@ app.controller('oggettoController', [
 		
 		$scope.aggiungiOggetto = function() {
 			var oggetto = {};
+			var attributiValore = {};
+			userService.getUtente($window.sessionStorage.getItem('USER_USERNAME')).then(
+					function(response) {
+						if (response.esito == "OK") {
+							oggetto.utente = response.data;
+						} else {
+							$window.alert("Errore");
+						}
+					});
 			oggetto.nome = $scope.nomeOggetto;
 			oggetto.categoria = $scope.categoria;
+			for (i = 0; i < $scope.listAttributi.length; i++) { 
+				var string = "attributo_"+i;
+				attributiValore[$scope.listAttributi[i].idAttributo] = $scope.$eval(string);
+			}
+			oggetto.attributiValore = attributiValore;
+			
 			oggettoService.saveOggetto(oggetto).then(
 					function(response) {
 						if (response.esito == "OK") {
