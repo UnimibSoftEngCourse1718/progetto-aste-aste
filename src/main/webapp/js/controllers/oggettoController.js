50ab7e6161a228072cd931089f8986e9b68427c1
Unknown <s.fratea2@campus.unimib.it>
2018-06-09 18:06:51 +0200

    Create Oggetto
    fix create Oggetto + findAll oggetto in home page

--- src/main/webapp/js/controllers/oggettoController.js
+++ src/main/webapp/js/controllers/oggettoController.js
@@ -25,32 +25,34 @@ app.controller('oggettoController', [
 		};	
 		
 		$scope.aggiungiOggetto = function() {
+			var body = {};
 			var oggetto = {};
 			var attributiValore = {};
 			userService.getUtente($window.sessionStorage.getItem('USER_USERNAME')).then(
-					function(response) {
-						if (response.esito == "OK") {
-							oggetto.utente = response.data;
-						} else {
-							$window.alert("Errore");
+				function(response) {
+					if (response.esito == "OK") {
+						oggetto.utente = response.data;
+						oggetto.nome = $scope.nomeOggetto;
+						oggetto.categoria = $scope.categoria;
+						for (i = 0; i < $scope.listAttributi.length; i++) { 
+							var string = "attributo_"+i;
+							attributiValore[$scope.listAttributi[i].idAttributo] = $("#"+string).val();
 						}
-					});
-			oggetto.nome = $scope.nomeOggetto;
-			oggetto.categoria = $scope.categoria;
-			for (i = 0; i < $scope.listAttributi.length; i++) { 
-				var string = "attributo_"+i;
-				attributiValore[$scope.listAttributi[i].idAttributo] = $scope.$eval(string);
-			}
-			oggetto.attributiValore = attributiValore;
-			
-			oggettoService.saveOggetto(oggetto).then(
-					function(response) {
-						if (response.esito == "OK") {
-							$window.alert("Aggiunto");
-						} else {
-							$window.alert("Errore");
-						}
-					});
+						body.attributiValore = attributiValore;
+						body.oggetto = oggetto;
+						oggettoService.saveOggetto(body).then(
+								function(response) {
+									if (response.esito == "OK") {
+										$window.alert("Aggiunto");
+										$window.location.href = '#/profilo';
+									} else {
+										$window.alert("Errore");
+									}
+								});
+					} else {
+						$window.alert("Errore");
+					}
+				});
 		};
 	}
 ]);
