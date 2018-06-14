266b1fefb664385e6c2b484ab446c1070e644e2f
Simone <s.elli@campus.unimib.it>
2018-06-14 15:51:40 +0200

    Dispaly max offer into page "detteglio"

--- src/main/webapp/js/services/offerteAttiveService.js
+++ src/main/webapp/js/services/offerteAttiveService.js
@@ -1,4 +1,5 @@
 app.service('offerteAttiveService',['$http', function ($http) {
+	
     //Restituisce le offerte attive di un utente
     this.offerteAttive = function offerteAttive(id){
         return $http({
@@ -19,5 +20,19 @@ app.service('offerteAttiveService',['$http', function ($http) {
         }).then( function(response){
             return response.data;
         });
+    }
+    
+    // Funzione per trovare l' offerta massima corrente
+    this.findMaxOffertaByOggetto = function findMaxOffertaByOggetto(oggetto) {
+    	
+    	return $http({
+            method: 'POST',
+            url: 'rest/offerta/findMaxOfferteByOggetto',
+            data: oggetto,
+            headers: 'Accept:application/json'
+          }).then( function(response){
+              return response.data;
+          });
+    	
     }
 }]);
