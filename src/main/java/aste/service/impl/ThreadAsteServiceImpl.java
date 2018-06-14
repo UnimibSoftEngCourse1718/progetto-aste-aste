60e43d9a9644b4bfb90edb12e94d19ede4f6973b
Stefano <s.fratea2@campus.unimib.it>
2018-06-14 18:22:05 +0200

    fix asta

--- src/main/java/aste/service/impl/ThreadAsteServiceImpl.java
+++ src/main/java/aste/service/impl/ThreadAsteServiceImpl.java
@@ -6,8 +6,10 @@ import org.springframework.stereotype.Service;
 import aste.model.Offerta;
 import aste.model.Offerta.Stato;
 import aste.model.Oggetto;
+import aste.model.Utente;
 import aste.service.OffertaService;
 import aste.service.ThreadAsteService;
+import aste.service.UtenteService;
 
 @Service
 public class ThreadAsteServiceImpl implements ThreadAsteService {
@@ -15,12 +17,18 @@ public class ThreadAsteServiceImpl implements ThreadAsteService {
 	@Autowired
 	OffertaService offertaService;
 	
+	@Autowired
+	UtenteService utenteService;
+	
 	public void runThread(Oggetto oggetto) throws InterruptedException {
 		Thread.sleep(oggetto.getTempoAsta()*60*1000);
 		Offerta offerta = offertaService.findFirstByIdOggettoOrderByIdOffertaDesc(oggetto);
 		offerta.setStato(Stato.VENDUTO);
 		offertaService.updateOfferta(offerta);
 		offertaService.deleteByIdOggettoAndStato(oggetto, Stato.ATTIVO);
+		Utente utente = oggetto.getUtente();
+		utente.setCredito(utente.getCredito() + offerta.getImporto());
+		utenteService.updateUser(utente);
 	}
 
 }
